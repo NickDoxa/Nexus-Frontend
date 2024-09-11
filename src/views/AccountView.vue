335a531f8df1efadb296d0a0c123a47275ea5c20
Nick Doxa <77412044+NickDoxa@users.noreply.github.com>
2024-09-11 18:43:53 -0400

    Updated user retrieval logic

--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 import { useAuth0 } from '@auth0/auth0-vue'
-import { onBeforeMount } from 'vue'
 import { useRouter } from 'vue-router'
+import { onBeforeMount, ref } from 'vue'
 
 const {logout} = useAuth0()
-onBeforeMount(() => {
-  const {isAuthenticated} = useAuth0()
-  const router = useRouter()
-  if (!isAuthenticated) router.push('/')
-})
+const router = useRouter()
+const userid = ref<String>()
+const username = ref<String>()
+const email = ref<String>()
+const picture_src = ref<String>()
 
 const handleLogout = () => {
   logout({
@@ -17,10 +17,26 @@ const handleLogout = () => {
     }
   })
 }
+
+onBeforeMount(async () => {
+  const {isAuthenticated} = useAuth0()
+  if (!isAuthenticated.value) await router.push('/')
+  else {
+    username.value = useAuth0().user.value.name ?? ''
+    email.value = useAuth0().user.value.email ?? ''
+    userid.value = useAuth0().user.value.sub ?? ''
+    picture_src.value = useAuth0().user.value.picture ?? ''
+  }
+})
+
 </script>
 
 <template>
   <h1><a href="#" @click="handleLogout">account</a></h1>
+  <code>{{username}}</code><br/>
+  <code>{{email}}</code><br/>
+  <code>{{userid}}</code><br/>
+  <code>{{picture_src}}</code>
 </template>
 
 <style scoped>
