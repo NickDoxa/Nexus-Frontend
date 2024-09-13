d74278cc7ea612d0451f721a26d00987bf1bf636
Nick Doxa <77412044+NickDoxa@users.noreply.github.com>
2024-09-13 00:11:26 -0400

    updated user retrieval logic

--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -1,53 +1,81 @@
 <script setup lang="ts">
-import { useAuth0 } from '@auth0/auth0-vue'
+import { useAuth0, User } from '@auth0/auth0-vue'
 import { useRouter } from 'vue-router'
 import { onBeforeMount, reactive, ref } from 'vue'
 import { UserDto } from '@/common/dto/user.dto'
 import { HttpAPI } from '@/common/API'
 import LogoutButton from '@/components/buttons/LogoutButton.vue'
+import ProfileCard from '@/components/cards/ProfileCard.vue'
+import { CardDto } from '@/common/dto/card.dto'
 
 const validated = ref<Boolean>(false)
 const router = useRouter()
+
+const setUserValuesByAuth = (userValues) => {
+  user.authId = userValues.sub ?? ''
+  user.username = userValues.name ?? ''
+  user.email = userValues.email ?? ''
+  user.picture = userValues.picture ?? ''
+}
+
+const getDefaultCard: CardDto = () => {
+  return {
+    bio: 'Not Set',
+    backgroundColorHex: '#000000',
+    textColorHex: '#FFFFFF',
+    favoriteGame: 'Not Set',
+    platform: 'Not Set'
+  }
+}
+
+const getDefaultUser: UserDto = () => {
+  return {
+    authId: user.authId,
+    username: user.username,
+    email: user.email,
+    picture: user.picture,
+    card: getDefaultCard()
+  }
+}
+
+const setUserValues = (userValues: UserDto) => {
+  user.authId = userValues.authId ?? ''
+  user.username = userValues.username ?? ''
+  user.email = userValues.email ?? ''
+  user.picture = userValues.picture ?? ''
+  user.card = userValues.card ?? undefined
+}
+
 const user = reactive<UserDto>({
   authId: '',
   username: '',
   email: '',
-  picture: ''
+  picture: '',
+  card: getDefaultCard()
 })
 
 onBeforeMount(async () => {
-  const {isAuthenticated} = useAuth0()
+  const { isAuthenticated } = useAuth0()
   if (!isAuthenticated.value) {
     await router.push('/')
     return
   }
-  const userValue = useAuth0().user.value
-  user.authId = userValue.sub ?? ''
-  user.username = userValue.name ?? ''
-  user.email = userValue.email ?? ''
-  user.picture = userValue.picture ?? ''
+  setUserValuesByAuth(useAuth0().user.value)
   try {
     const exists_response = await HttpAPI.userExists(user.authId)
     const exists_data:UserDto = exists_response.data
     if (exists_data.authId === null || exists_data.authId === '') {
-      const transferDTO = {
-        authId: user.authId,
-        username: user.username,
-        email: user.email,
-        picture: user.picture
-      }
-      const create_response = await HttpAPI.createNewUser(transferDTO)
-      const create_data = create_response.data
+      const userDto = getDefaultUser(getDefaultCard())
+      const create_response = await HttpAPI.createNewUser(userDto)
+      const create_data:UserDto = create_response.data
+      setUserValues(create_data)
       validated.value = create_data.authId !== null && create_data.authId !== ''
     } else {
-      user.authId = exists_data.authId
-      user.username = exists_data.username
-      user.email = exists_data.email
-      user.picture = exists_data.picture
+      setUserValues(exists_data)
       validated.value = true
     }
   } catch (error) {
-    console.log("Request Error!")
+    console.log('Request Error!')
     console.log(error)
   }
 })
@@ -55,10 +83,19 @@ onBeforeMount(async () => {
 
 <template>
   <!-- VALIDATED CONTENT -->
-  <LogoutButton :validated="validated"/>
+  <div
+    class="transition-in animate-fadein centered-content pushed-down"
+    :class="{ hidden: !validated, shown: validated }"
+  >
+    <ProfileCard :user="user" :validated="validated"/>
+    <LogoutButton />
+  </div>
+
   <!-- LOADER UNTIL VALIDATED -->
-  <div class="centered-content pushed-down transition-out"
-       :class="{'hidden' : validated, 'shown' : !validated}">
-    <img src="@/assets/images/loading.svg" alt="loading"/>
+  <div
+    class="centered-content pushed-down transition-out"
+    :class="{ hidden: validated, shown: !validated }"
+  >
+    <img src="@/assets/images/loading.svg" alt="loading" />
   </div>
 </template>
