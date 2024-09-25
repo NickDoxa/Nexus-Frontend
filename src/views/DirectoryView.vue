5656a707ce5cb2d8c1983185651f99105a263b0a
Nick Doxa <77412044+NickDoxa@users.noreply.github.com>
2024-09-25 18:04:59 -0400

    Updated directory view

--- src/views/DirectoryView.vue
+++ src/views/DirectoryView.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import FadedComponent from '@/components/animations/FadedComponent.vue'
-
-
+import DirectoryPage from '@/components/groups/DirectoryPage.vue'
+import SearchBar from '@/components/forms/SearchBar.vue'
+import { faSearch } from '@fortawesome/free-solid-svg-icons/faSearch'
 </script>
 <template>
   <div class="centered-content pushed-down">
     <FadedComponent>
+      <SearchBar :use-icon="true" :icon="faSearch" />
       <ScrollPanel style="height:40rem;">
-
+        <DirectoryPage :size="10"/>
       </ScrollPanel>
     </FadedComponent>
   </div>
