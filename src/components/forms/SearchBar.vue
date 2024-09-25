3a48c6ac22b9d3cc024812461ae79183d05b7cab
Nick Doxa <77412044+NickDoxa@users.noreply.github.com>
2024-09-25 18:05:06 -0400

    Added search bar for directory

---
+++ src/components/forms/SearchBar.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+import { ref } from 'vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+const props = defineProps<{
+  useIcon: Boolean
+  icon: Object
+}>()
+
+const emits = defineEmits<{
+  updateSearch: [search: string]
+}>()
+
+const currentSearch = ref<string>('')
+
+</script>
+
+<template>
+  <div class="centered-content search-bar-box">
+    <FloatLabel>
+      <InputText type="text" v-model="currentSearch" size="large" id="search-bar-input" style="width: 40rem;"/>
+      <label for="search-bar-input">Search <FontAwesomeIcon :icon="props.icon" /></label>
+    </FloatLabel>
+  </div>
+</template>
+
+<style scoped>
+.search-bar-box {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
+</style>
